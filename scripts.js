8ad57c1d38ae93ec8e37792e7afe19ca899c875e
Gabryelle SS <gabryelle-gaby@hotmail.com>
2019-02-15 09:22:54 -0200

    Adicionando nova alteração

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const menuHamburguer = document.querySelector('.menu-hamburguer');
 const navMenuHamburguer = document.querySelector('.nav-menuHamburguer');
 
 const ativarBotao = function(){
-const navMenuHamburguer = document.querySelector('.nav-menuHamburguer');
+    navMenuHamburguer.classList.toggle('.nav-menuHamburguer');
     navMenuHamburguer.classList.toggle('abrirMenuHamburguer')
 }
 
